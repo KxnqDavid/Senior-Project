ad7279ea986a65bbf9d85a6d1cb3867c6febce3a
KxnqDavid <david.quiralte03@gmail.com>
2024-02-29 16:07:37 -0600

    David

--- SeniorProjectGroup4/MainWindow.xaml.cs
+++ SeniorProjectGroup4/MainWindow.xaml.cs
@@ -1,6 +1,6 @@
 ﻿using System.Text;
 using System.Windows;
-using System.Windows.Forms;
+using Microsoft.Windows.Compatibility;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
@@ -21,11 +21,11 @@ namespace SeniorProjectGroup4
     {
         public MainWindow()
         {
-            InitializeComponent();
             Console.WriteLine("Hello World!");
 
+
         }
-        private void SelectDownloadLocation_Click(object sender, RoutedEventArgs e)
+        private void ChangeDirectory_Click(object sender, RoutedEventArgs e)
         {
             FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
 
