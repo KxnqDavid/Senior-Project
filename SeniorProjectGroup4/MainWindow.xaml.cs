25eb4f7c675cd0ad4aac21424a8e7d6d11604d35
KxnqDavid <david.quiralte03@gmail.com>
2024-02-29 14:42:12 -0600

    David

--- SeniorProjectGroup4/MainWindow.xaml.cs
+++ SeniorProjectGroup4/MainWindow.xaml.cs
@@ -8,6 +8,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System;
+using System.Diagnostics;
 
 namespace SeniorProjectGroup4
 {
@@ -20,6 +22,8 @@ namespace SeniorProjectGroup4
         {
             InitializeComponent();
             Console.WriteLine("Hello World!");
+            
+
         }
     }
 }
